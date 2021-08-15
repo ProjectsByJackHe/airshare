9530aef08d4eaefc6ee8e5bb69084f508c53f631
Jack He <jackhe2345@gmail.com>
2021-08-15 15:11:50 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 const handler = require('serve-handler');
-const http = require('http');
 const arg = process.argv.slice(2)
-const path = arg[0] || "/"
-const port = arg[1] || 80
+const sharepath = arg[0] || "./"
+const port = arg[1] || 1000
+const os = require('os')
+const express = require('express')
+const app = express() 
+const fs = require('fs')
+ 
 
 
-console.log("sharing path: " + path) 
-console.log("on port: " + port)
+const interfaces = os.networkInterfaces();
 
-const server = http.createServer((request, response) => {
-  // You pass two more arguments for config and middleware
-  // More details here: https://github.com/vercel/serve-handler#options
-  return handler(request, response, {
-    "public": path 
-  });
+const getNetworkAddress = () => {
+	for (const name of Object.keys(interfaces)) {
+		for (const interface of interfaces[name]) {
+			const {address, family, internal} = interface;
+      console.log(address)
+			if (family === 'IPv4' && !internal) {
+				return address;
+			}
+		}
+	}
+}; 
+
+app.use(async (req, res, next) => {
+  if (req.query.type === "files") {
+    return handler(req, res, {
+      "public": sharepath 
+    });
+  }
+  next() 
 })
 
-server.listen(port, '0.0.0.0', () => {
+app.use((req, res) => {
+  res.sendFile("./controlplane/index.html")
+})
+
+app.listen(port, '0.0.0.0', () => {
   console.log('Running at http://localhost:' + String(port));
 });
