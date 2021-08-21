4117158392cd534fc08fdf4d5ee78e50a31a4dbb
Jack He <jackhe2345@gmail.com>
2021-08-21 12:53:14 -0700

    change network stuff

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const handler = require('serve-handler');
 const arg = process.argv.slice(2)
 const path = require('path')
-const sharepath = arg[0] || "./"
+const sharepath = arg[0] || "../"
 const port = arg[1] || 1000
 const os = require('os')
 const express = require('express')
 const app = express() 
 const fs = require('fs')
 const bodyParser = require('body-parser')
-// single key will be used to represent internal memory cache
-// key: "nodes", value: JSON
+const cors = require('cors')
 const NodeCache = require("node-cache"); 
 const { exit } = require('process');
+// single key will be used to represent internal memory cache
+// key: "nodes", value: JSON
 const myCache = new NodeCache() 
 
 // utility functions 
@@ -32,8 +33,11 @@ try {
   // initialize node.json as current node since we don't know the state of all other hosts on the network yet. 
   const currentHostIp = String(getNetworkAddress()); 
   const currentDate = new Date()
+
+  // default metadata: hostname + os type (windows / mac / linux)
+  const defaultName = os.hostname() + " --- " + os.type()
   fs.writeFileSync("./var/nodes.json", JSON.stringify({
-    "nodes" : [{ "ip" : currentHostIp + ":" + port, "metadata" : "(this machine)", "creationDate": currentDate }]
+    "nodes" : [{ "ip" : currentHostIp + ":" + port, "metadata" : defaultName, "creationDate": currentDate }]
   }))
 
 } catch (err) {
@@ -43,9 +47,16 @@ try {
 
 app.use(bodyParser.json())
 
+app.use(cors())
+
+app.get("/exist", (req, res) => {
+  res.status(200).send("exists")
+})
+
 app.post("/node/update", (req, res) => {
   if (myCache.get("nodes")) myCache.del("nodes"); 
   let newNodes = req.body 
+  // newNodes should be { "nodes" : [{...}, {...}] }
   try {
     if (newNodes.nodes.length) {
       fs.writeFileSync("./var/nodes.json", newNodes)
@@ -65,6 +76,7 @@ app.get("/node/data", (req, res) => {
       nodes = fs.readFileSync("./var/node.json") 
       myCache.set("nodes", nodes) 
     }
+
     res.json(JSON.stringify(nodes))
   } catch (err) {
     res.status(500).send("get /node/data: " + String(err)) 
